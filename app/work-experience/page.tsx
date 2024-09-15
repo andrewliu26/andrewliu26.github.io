032d2fc1f73a651119d084800fecddb4b9ef2493
andrewliu26 <aliu26@bu.edu>
2024-09-15 17:42:29 -0400

    new template

---
+++ app/work-experience/page.tsx
@@ -0,0 +1,10 @@
+export default function WorkExperience() {
+    return (
+        <section>
+            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+                Work Experience
+            </h1>
+            <p className="mb-4">The page you are looking for does not exist.</p>
+        </section>
+    )
+}
